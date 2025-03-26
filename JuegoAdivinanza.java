1e54deac0b0f2ea948eb93f6f6e0b56ffaac41c4
David Ramirez Barrera <david.allbella@gmail.com>
2025-03-26 13:04:15 -0600

    Create JuegoAdivinanza.java
    Estructura general añadida.

---
+++ JuegoAdivinanza.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JuegoAdivinanza {
+    public static void main(String[] args) {
+        // aqui se genera el numero aleatorio
+        
+        // parametros par la logica
+
+        
+	// mensajes del menu
+        
+        
+	//while
+      
