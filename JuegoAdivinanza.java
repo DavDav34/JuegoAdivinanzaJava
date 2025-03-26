cbd0497aa27bff9fce170236ba893cd03633a67d
David Ramirez Barrera <david.allbella@gmail.com>
2025-03-26 13:17:53 -0600

    Update JuegoAdivinanza.java
    Actualizacion de la estructura. Lista para trabajar sobre ella.

--- JuegoAdivinanza.java
+++ JuegoAdivinanza.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 
 public class JuegoAdivinanza {
     public static void main(String[] args) {
-        // aqui se genera el numero aleatorio
+    // aqui se genera el numero aleatorio
         
-        // parametros par la logica
+        
+    // parametros par la logica
+        int intentosMaximos = 10;
+        int intentosRealizados = 0;
+        
+        Scanner scanner = new Scanner(System.in);
 
         
 	// mensajes del menu
-        
+        System.out.println("HOLAaaa VAMOS A JUGAR");
+        System.out.println("Adivina un numero entre 1 y 100");
+        System.out.println("Tienes " + intentosMaximos + " intentos");
         
 	//while
+        while(intentosRealizados < intentosMaximos) {
+            
+		}
       
